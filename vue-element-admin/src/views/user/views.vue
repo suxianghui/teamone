ce052531311f158646ffbce6132bec44aa0523e0
yrb <978653087@qq.com>
2019-04-23 10:58:50 +0800

    列表渲染，存在分页bug

--- vue-element-admin/src/views/user/views.vue
+++ vue-element-admin/src/views/user/views.vue
@@ -2,148 +2,134 @@
   <div class="wrap">
     <!-- 头部标题 -->
     <p>用户展示</p>
-    <!-- 大容器包裹盒子 -->
-    <!-- <div class="showBox"> -->
-      <!-- 顶部面包屑导航 -->
-      <div class="nav">
-        <!-- <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
-          <el-tab-pane label="用户管理" name="first">
-            <span class="title">用户管理</span>
-          </el-tab-pane>
-          <el-tab-pane label="身份数据" name="second">
-            <span class="title">配置管理</span>
-          </el-tab-pane>
-          <el-tab-pane label="api接口权限" name="third">
-            <span class="title">角色管理</span>
-          </el-tab-pane>
-          <el-tab-pane label="身份和qpi接口关系" name="fourth">
-            <span class="title">身份和qpi接口关系</span>
-          </el-tab-pane>
-          <el-tab-pane label="视图接口权限" name="fifth">
-            <span class="title">视图接口权限</span>
-          </el-tab-pane>
-          <el-tab-pane label="身份和视图权限关系" name="sixth">
-            <span class="title">身份和视图权限</span>
-          </el-tab-pane>
-        </el-tabs> -->
-         <span
-          :class="idx === index?'blue':''"
-          @click="changeTitle(index)"
-          :data-val="index"
-          v-for="(item,index) in nav"
+    <!-- 顶部面包屑导航 -->
+    <div class="nav">
+      <span
+        :class="idx === index?'blue':''"
+        @click="changeTitle(index)"
+        :data-val="index"
+        v-for="(item,index) in nav"
+        :key="index"
+      >{{item}}</span>
+    </div>
+    <h2>{{txt}}</h2>
+    <!-- 表格 -->
+    <div>
+      <el-table :data="data" style="width: 100%">
+        <el-table-column
+          v-for="(item,index) in tableData[idx]"
           :key="index"
-        >{{item}}</span>
-      </div>
-       <h2>{{txt}}</h2>
-      <!-- 表格 -->
-      <div>
-        <el-table
-          :data="tableData"
-          style="width: 100%"
-        >
-          <el-table-column
-            v-for = "(item,index) in tabNav" :key = "index"
-            prop="date"
-            :label="item"
-            :style="{ width:'100%'/tabNav.length }"
-          />
-          <!-- <el-table-column
-          v-for = "(item,index) in tabName" :key = "index"
-            prop="name"
-            :label=item
-            :style="{ width:'100%'/tabName.length } "
-          /> -->
-          <!-- <el-table-column
-            prop="address"
-            label="地址"
-          /> -->
-        </el-table>
-      </div>
-      <!-- 分页 -->
-      <div class="pagination">
-        <el-pagination
-          @current-change = "handleChangePage"
-          background
-          layout="prev, pager, next"
-          :total="total"
+          :prop="item"
+          :label="tabNav[idx][index]"
+          
+          :style="{width:'100%'/tabNav[idx].length }"
         />
-      </div>
+        <!-- label : 头部导航 -->
+        <!-- prop : 每一项的数据 -->
+      </el-table>
+    </div>
+    <!-- 分页 -->
+    <div class="pagination">
+      <el-pagination
+        @current-change="handleChangePage"
+        background
+        layout="prev, pager, next"
+        :total="data.length"
+        :page-size="10"
+      />
+    </div>
     <!-- </div> -->
   </div>
 </template>
 
 <script>
-
-import { mapState,mapMutations,mapActions } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
   data() {
     return {
-      nav:[
-        "用户数据","身份数据","api接口权限","身份和api接口关系","视图接口权限","身份和视图权限关系"
-        ],
-      tabNav:[
-        "用户名","密码","身份"
+      nav: [
+        "用户数据",
+        "身份数据",
+        "api接口权限",
+        "身份和api接口关系",
+        "视图接口权限",
+        "身份和视图权限关系"
       ],
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      },
-      {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      },
-      {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      },
-      {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      idx:0,
-      txt:'用户数据',
-      total:100
-    }
+      tabNav: [
+        ["用户名", "密码", "身份"],
+        ["身份名称"],
+        ["api权限名称", "api权限接口", "api权限方法"],
+        ["身份名称", "api权限名称", "api权限url", "api权限方法"],
+        ["视图权限名称", "视图id"],
+        ["身份", "视图名称", "视图id"]
+      ],
+      tableData: [
+        ["user_name", "user_pwd", "identity_text"],
+        ["identity_text"],
+        ["api_authority_text", "api_authority_url", "api_authority_method"],
+        [
+          "identity_text",
+          "api_authority_text",
+          "api_authority_url",
+          "api_authority_method"
+        ],
+        ["view_authority_text", "view_id"],
+        ["identity_text", "view_authority_text", "view_id"]
+      ],
+      idx: 0,
+      txt: "用户数据",
+      page: 1, //第几页
+      // totals:0,
+      pageTit: [
+        "userData",
+        "identitysData",
+        "apiAuthoritysData",
+        "identityApiAuthorityRelationsData",
+        "viewAuthoritysData",
+        "identityViewAuthorityRelationsData"
+      ],
+      defaultPage: 1
+    };
   },
-  computed:{
+  computed: {
     ...mapState({
       //获取用户数据
-      userDatas:state => state.viewUsers.userDatas,
+      userDatas: state => state.viewUsers.userDatas,
       //获取身份数据
-      identitysData:state => state.viewUsers.identitysData,
+      identitysData: state => state.viewUsers.identitysData,
       //获取api接口权限数据
-      apiAuthoritysData:state => state.viewUsers.apiAuthoritysData,
+      apiAuthoritysData: state => state.viewUsers.apiAuthoritysData,
       //获取视图权限数据
-      viewAuthoritysData:state => state.viewUsers.viewAuthoritysData,
+      viewAuthoritysData: state => state.viewUsers.viewAuthoritysData,
       //获取身份和api权限关系
-      identityViewAuthorityRelationsData:state => state.viewUsers.identityViewAuthorityRelationsData,
+      identityViewAuthorityRelationsData: state =>
+        state.viewUsers.identityViewAuthorityRelationsData,
       //获取身份和api权限关系
-      identityApiAuthorityRelationsData:state => state.viewUsers.identityApiAuthorityRelationsData   
+      identityApiAuthorityRelationsData: state =>
+        state.viewUsers.identityApiAuthorityRelationsData,
+
+      totals: state => state.viewUsers.total,
+      data: state => state.viewUsers.data,
+      pageSize: state => state.viewUsers.pageSize,
+      userData: state => state.viewUsers.userData
     })
   },
-  created(){
-    console.log('set..',this.setUserData());
-  },
-  methods:{
-    ...mapMutations({
+  created() {
+    this.setUserData();
+    // console.log(this.data,'set')
+    this.setidentity();
+    this.setApiAuthority();
+    this.setIdentityApiAuthorityRelation();
 
+    this.setViewAuthority();
+
+    this.setidentityViewAuthorityRelation();
+  },
+  methods: {
+    ...mapMutations({
+      tableList: "viewUsers/tableList"
     }),
+
     ...mapActions({
       //展示用户数据
       setUserData: "viewUsers/setUserData",
@@ -152,57 +138,45 @@ export default {
       //展示api接口数据
       setApiAuthority: "viewUsers/setApiAuthority",
       //展示身份和api权限关系
-      setIdentityApiAuthorityRelation:"viewUsers/setIdentityApiAuthorityRelation",
+      setIdentityApiAuthorityRelation:
+        "viewUsers/setIdentityApiAuthorityRelation",
       //获取视图权限
       setViewAuthority: "viewUsers/setViewAuthority",
       //展示身份和视图权限关系
-      setidentityViewAuthorityRelation:"viewUsers/setidentityViewAuthorityRelation"
+      setidentityViewAuthorityRelation:
+        "viewUsers/setidentityViewAuthorityRelation"
     }),
-    
+
     //分页
-    handleChangePage(){
-      console.log(111111111)
+    handleChangePage(val) {
+      this.defaultPage = val;
+      this.tableList({
+        data: this.pageTit[this.idx],
+        pages: this.page
+      });
     },
+
     //点击切换标题
-    changeTitle(idx){
+    changeTitle(idx) {
+      // console.log(this.pageTit[idx], 787878);
+      this.defaultPage = 1;
+      this.tableList({
+        idx,
+        data: this.pageTit[idx],
+        pages: this.page
+      });
       this.idx = idx;
-      this.txt = this.nav[idx];
-      if( idx === 0 ){
-        //标题项
-        this.tabNav = ['用户名','密码','身份'];
-        //展示用户数据
-        this.setUserData()
-      }else if( idx === 1 ){
-        this.tabNav = ['身份名称'];
-        //展示身份数据
-        this.setidentity()
-      }else if( idx === 2 ){
-        this.tabNav = ['api权限名称','api权限url','api权限方法']
-        //展示api接口数据
-        this.setApiAuthority()
-      }else if( idx === 3 ){
-        this.tabNav = ['身份名称','api权限名称','api权限url','api权限方法']
-        //展示身份和api权限关系
-        this.setIdentityApiAuthorityRelation()
-      }else if( idx === 4 ){
-        this.tabNav = ['视图权限名称','视图id']
-        //获取视图权限
-        this.setViewAuthority()
-      }else if( idx === 5 ){
-        this.tabNav = ['身份','视图名称','视图id']
-       //展示身份和视图权限关系
-        this.setidentityViewAuthorityRelation()
-      }
+      this.tit = this.nav[idx];
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.wrap{
+.wrap {
   width: 100%;
   height: 100%;
-  padding:0 24px;
+  padding: 0 24px;
   box-sizing: border-box;
   > p {
     font-size: 22px;
@@ -211,26 +185,26 @@ export default {
   h2 {
     font-weight: 500;
   }
-  .nav{
-    span{
+  .nav {
+    span {
       font-size: 14px;
-      border:solid 1px #ccc;
-      color:#ccc;
+      border: solid 1px #ccc;
+      color: #ccc;
       background: #fff;
-      padding:0 10px;
+      padding: 0 10px;
       cursor: pointer;
       display: inline-block;
       height: 30px;
       line-height: 30px;
-    } 
+    }
     .blue {
-        border: 1px solid #0139fd;
-        color: #0139fd;
+      border: 1px solid #0139fd;
+      color: #0139fd;
     }
   }
-  .pagination{
+  .pagination {
     position: absolute;
-    right:0;
+    right: 0;
   }
- }
+}
 </style>
