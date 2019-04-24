bbd63c42d0cb5d97415016a220a0fe643ab96baf
yuanda <1754906214@qq.com>
2019-04-24 11:12:50 +0800

    ...

--- vue-element-admin/src/store/modules/user.js
+++ vue-element-admin/src/store/modules/user.js
@@ -46,30 +46,6 @@ const actions = {
     let data = await getInfo();
     commit('SET_USERINFO', data.data);
     return data.data;
-    // return new Promise((resolve, reject) => {
-    //   getInfo(state.token).then(response => {
-    //     const { data } = response
-
-    //     if (!data) {
-    //       reject('Verification failed, please Login again.')
-    //     }
-
-    //     const { roles, name, avatar, introduction } = data
-
-    //     // roles must be a non-empty array
-    //     if (!roles || roles.length <= 0) {
-    //       reject('getInfo: roles must be a non-null array!')
-    //     }
-
-    //     commit('SET_ROLES', roles)
-    //     commit('SET_NAME', name)
-    //     commit('SET_AVATAR', avatar)
-    //     commit('SET_INTRODUCTION', introduction)
-    //     resolve(data)
-    //   }).catch(error => {
-    //     reject(error)
-    //   })
-    // })
   },
 
   // user logout
